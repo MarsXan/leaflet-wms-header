ecd42bc53f7a365f947d5991d6e688784e35b54e
Mohsen Mokhtari <mr.mars1992@gmail.com>
2019-12-10 14:17:06 +0330

    fix Cannot find name 'WMSHeader' in type script

--- index.d.ts
+++ index.d.ts
@@ -1,24 +1,19 @@
 import * as L from 'leaflet';
 
-
 declare module 'leaflet' {
-
-    namespace TileLayer {
-
-        export class WMSHeader extends WMS {
-            constructor(
-                baseUrl: string,
-                options: WMSOptions,
-                header: {header: string, value: string}[]
-            );
-        }
-
-        export function wmsHeader(
-            baseUrl: string,
-            options: WMSOptions,
-            header: {header: string, value: string}[]
-        ): WMSHeader;
-
+  namespace TileLayer {
+    export class WMSHeader extends WMS {
+      constructor(
+        baseUrl: string,
+        options: WMSOptions,
+        header: { header: string; value: string }[]
+      );
     }
 
+    export function wmsHeader(
+      baseUrl: string,
+      options: WMSOptions,
+      header: { header: string; value: string }[]
+    ): L.TileLayer.WMSHeader;
+  }
 }
